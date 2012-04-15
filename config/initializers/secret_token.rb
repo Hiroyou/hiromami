811fa7ca19cd7d840fc14b48f9aaa3137bee128c
Hiroyou <hyshhryk@gmail.com>
2012-04-15 20:27:24 +0900

    add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hiromami::Application.config.secret_token = '771feea6e2f15210dc19d6dcee013c34caca2d32d251a2e7ed435bd786292a5e193744707faf4f12b768a5e293692e6f4f06496c5eab3505dd191461d7f736b0'
